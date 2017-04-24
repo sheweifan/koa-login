a9eb0fc3ca79029b7f0676efa8044e76003a8fa7
weifan she <923986234@qq.com>
2017-04-24 18:01:16 +0800

    add

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 'use strict';
 var Koa = require('koa');
 
-var Router = require('koa-router')
-var session = require('koa-session')
-var bodyParser = require('koa-bodyparser')
-var ejs = require('koa-ejs')
+var Router = require('koa-router')();
+var session = require('koa-session');
+var bodyParser = require('koa-bodyparser');
+var ejs = require('koa-ejs');
+var views = require('koa-views');
+var moment = require('moment');
 
 var app = new Koa();
 
+app.use(views(__dirname + '/app/views', {
+  extension: 'ejs',
+  locals: {
+    moment: moment
+  }
+}))
 
+Router.get('/',function *(next) {
+    // this.body = 'index';
+    yield this.render('pages/index', {
+      title: '首页'
+    })
+});
+
+app.use(Router.routes());
 
 
 app.listen(18080);
