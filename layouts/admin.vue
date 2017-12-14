2f306a32a3a7fae3278f3b36483c918811e53517
weifan she <923986234@qq.com>
2017-12-14 17:48:31 +0800

    layouts init

---
+++ layouts/admin.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  el-container.admin-container
+    //- el-header
+      //- navbar
+    el-container
+      el-aside(width="200px")
+        side
+      el-main
+        nuxt
+</template>
+
+<script>
+  import '../plugins/element-ui'
+  import side from '../components/admin/side'
+  import navbar from '../components/admin/navbar'
+  export default {
+    components: {
+      side,
+      navbar
+    }
+  }
+</script>
+
+<style lang="stylus">
+  *
+    line-height: 1.25
+  .admin-container
+    height: 100vh
+    .el-header, .el-footer
+      background-color: #e6e6e6
+      line-height: 60px
+
+    .el-aside
+      background-color: #E9EEF3
+      line-height: 200px
+
+    .el-main
+      // overflow: auto
+      // background-color: #
+      line-height: 160px
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div.page-404
+    div
+      p.title {{ error.statusCode }}
+      p.info {{ error.message }}
+      nuxt-link(to="/") back home page
+</template>
+
+<script>
+  export default {
+    props: ['error']
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .page-404
+    height: 100vh
+    box-sizing: border-box
+    padding-bottom: 200px
+    display: flex
+    justify-content: center
+    align-items: center
+    text-align: center
+    .title
+      font-size: 40px
+      line-height: 2
+</style>
