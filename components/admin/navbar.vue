de9d3263ce6f1ba79a855471ba1f19d7b904be88
weifan she <923986234@qq.com>
2017-12-14 17:47:30 +0800

    components init

---
+++ components/admin/navbar.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div.admin-navbar
+  span 十八匠
+  //- el-menu(mode="horizontal")
+  //-   el-menu-item(index="1") 处理中心
+  //-   el-menu-item(index="2") 处理中心
+  //-   el-menu-item(index="3") 处理中心
+  //-   el-menu-item(index="4") 处理中心
+  //- span admin
+</template>
+
+<script>
+</script>
+
+<style scoped lang="stylus">
+  .admin-navbar
+    display: flex
+    justify-content: space-between
+    align-items: center
+</style>
---
+++ components/admin/side.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  el-menu(
+    :router="true"
+    :default-active="navActive"
+  )
+    template(
+      v-for="(item, index) in nav"
+    )
+      el-menu-item(
+        v-if="!item.child"
+        :key="index"
+        :index="item.path"
+      )
+        i(:class="'el-icon-' + item.icon")
+        span {{ item.label }}
+      el-submenu(
+        v-else
+        :key="index"
+        :index="item.path"
+      )
+        template(slot="title")
+          i(:class="'el-icon-' + item.icon")
+          span {{ item.label }}
+        el-menu-item(
+          v-for="(citem, cindex) in item.child"
+          :key="index + '-' + cindex"
+          :index="citem.path"
+        ) {{ citem.label }}
+</template>
+
+<script>
+  const navConfig = [
+    {
+      label: '首页',
+      path: '/admin',
+      icon: 'menu'
+    },
+    {
+      label: '内容管理',
+      path: '1',
+      icon: 'document',
+      child: [
+        {
+          label: '首页',
+          path: '/admin/doc'
+        },
+        {
+          label: '设计大匠',
+          path: '1-2'
+        },
+        {
+          label: '新闻中心',
+          path: '1-3'
+        },
+        {
+          label: '加入我们',
+          path: '1-4'
+        }
+      ]
+    },
+    {
+      label: '账号管理',
+      path: '2',
+      icon: 'setting'
+    }
+  ]
+  export default {
+    data () {
+      return {
+        nav: navConfig,
+        navActive: this.$route.path
+      }
+    },
+    created () {
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+</style>
