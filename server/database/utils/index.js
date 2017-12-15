9ffb7ef091c8375180f65d1907c909921f12d65e
weifan she <923986234@qq.com>
2017-12-15 13:16:14 +0800

    add db until

---
+++ server/database/utils/index.js
@@ -0,0 +1,8 @@
+export const setMeta = function (next) {
+  if (this.isNew) {
+    this.meta.createdAt = this.meta.updatedAt = Date.now()
+  } else {
+    this.meta.updatedAt = Date.now()
+  }
+  next()
+}
