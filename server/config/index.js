5abb53e775b0af681b0f5eebde0b441eeb6195df
weifan she <923986234@qq.com>
2017-12-18 09:16:00 +0800

    add config

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+export default {
+  db: 'input_your_db_path',
+  qiniu: {
+    AK: 'input_your_qiniu_AccessKey',
+    SK: 'input_your_qiniu_SecretKey',
+    bucket: 'input_your_qiniu_Bucket_name'
+  }
+}
