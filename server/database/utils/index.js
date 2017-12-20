9734600d9e6e3913080ca7304c1b30248ca02cd7
weifan she <923986234@qq.com>
2017-12-20 08:55:36 +0800

    add getlist fn

--- server/database/utils/index.js
+++ server/database/utils/index.js
@@ -6,3 +6,26 @@ export const setMeta = function (next) {
   }
   next()
 }
+
+/**
+ * 分页方法
+ * @param {Number} pageIndex 页码
+ * @param {Number} pageSize 每页个数
+ * @param {Object} query 筛选项
+ * @param {String} populate 关联字段
+ */
+export const getList = async function (pageIndex = 1, pageSize = 10, query = {}, populate = '') {
+  const list = await this.find(query)
+    .populate(populate)
+    .skip(pageSize * (pageIndex - 1))
+    .limit(pageSize)
+    .exec()
+  const totalCount = await this.find(query)
+    .populate(populate)
+    .count()
+    .exec()
+  return {
+    list,
+    totalCount
+  }
+}
