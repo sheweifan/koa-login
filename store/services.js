91326188ce4585db090125a29ed257732a5afce6
weifan she <923986234@qq.com>
2017-12-27 17:54:24 +0800

    map list/put/del api methods

--- store/services.js
+++ store/services.js
@@ -5,8 +5,11 @@ import _isObject from 'lodash/isObject'
 import _isNumber from 'lodash/isNumber'
 import _isString from 'lodash/isString'
 import _compact from 'lodash/compact'
+import _forEach from 'lodash/forEach'
+import _capitalize from 'lodash/capitalize'
 
 const baseurl = ''
+// Graphql value parser
 const Gparse = (value) => {
   if (_isArray(value)) {
     return JSON.stringify(value)
@@ -20,6 +23,7 @@ const Gparse = (value) => {
     return null
   }
 }
+// Graphql stringify
 const Gstringify = (data) => {
   return `{
     ${_map(data, (value, key) => {
@@ -31,23 +35,11 @@ const Gstringify = (data) => {
 }
 
 class Services {
-  getDesigners ({pageIndex, keys, query}) {
-    return axios.get(baseurl + '/graphql', {
-      params: {
-        query: `{
-          designers (pageIndex: ${pageIndex}${query ? ',query:' + query : ''}) {
-            totalCount
-            list {
-              ${keys}
-            }
-          }
-        }`
-      }
-    })
-  }
+  // 获取7牛上传token
   getQiniuToken (key) {
     return axios.get(`${baseurl}/qiniu/getToken?key=${key}`)
   }
+  // 获得各种类型
   getTypes () {
     return axios.get(baseurl + '/graphql', {
       params: {
@@ -86,27 +78,57 @@ class Services {
       }
     })
   }
-  async putDesigner (data) {
-    const mutation = data._id
-    ? `mutation {
-      designerUpdate(data: ${Gstringify(data)}, _id: ${data._id})
-    }`
-    : `mutation {
-      designerCreate(data: ${Gstringify(data)})
-    }`
-    const ret = await axios.post(baseurl + '/graphql', {
-      query: mutation
+  // 数据接口
+  async _getLists (apiName, {pageIndex, keys, query}) {
+    const {data: {data: {data}}} = await axios.get(baseurl + '/graphql', {
+      params: {
+        query: `{
+          data: ${apiName}s (pageIndex: ${pageIndex}${query ? ',query:' + Gstringify(query) : ''}) {
+            totalCount
+            list {
+              ${keys}
+            }
+          }
+        }`
+      }
     })
-    return ret.data.data.designerUpdate || ret.data.data.designerCreate
+    return data
   }
-  async deleteDesigner (_id) {
+  // 更新或者创建数据
+  async _put (apiName, data) {
+    const mutation = data._id
+    ? `${apiName}Update(data: ${Gstringify(data)}, _id: ${data._id})`
+    : `${apiName}Create(data: ${Gstringify(data)})`
     const ret = await axios.post(baseurl + '/graphql', {
       query: `mutation {
-        designerRemove(_id: ${_id})
+        res: ${mutation}
       }`
     })
-    return ret.data.data.designerRemove
+    return ret.data.data.res
+  }
+  // 删除数据
+  async _del (apiName, _id) {
+    const ret = await axios.post(baseurl + '/graphql', {
+      query: `mutation {
+        res: ${apiName}Remove(_id: ${_id})
+      }`
+    })
+    return ret.data.data.res
   }
 }
+
+const apiNames = ['designer', 'map']
+_forEach(apiNames, item => {
+  const name = _capitalize(item)
+  Services.prototype[`get${name}s`] = function (data) {
+    return this._getLists(item, data)
+  }
+  Services.prototype[`put${name}`] = function (data) {
+    return this._put(item, data)
+  }
+  Services.prototype[`del${name}`] = function (_id) {
+    return this._del(item, _id)
+  }
+})
 
 export default new Services()
