690ec7d337a01a23ae8650760ddba67b945fec6f
weifan she <923986234@qq.com>
2017-12-29 09:59:37 +0800

    use component uploader && fix some bug

--- pages/admin/doc/designer.vue
+++ pages/admin/doc/designer.vue
@@ -10,7 +10,6 @@
     //-     el-input(type="search" placeholder="请输入搜索内容")
     //-   el-form-item
     //-     el-button(type="primary" size="medium") 搜索
-
     el-table(
       :data="designers"
       style="width: 100%"
@@ -83,9 +82,9 @@
             el-option(
               v-for="item in citys"
               v-if="item.leval === 1"
-              :key="item.name"
+              :key="item._id"
               :label="item.name"
-              :value="{_id: item._id, name: item.name}"
+              :value="item._id"
             )
         el-form-item(label="等级" prop="leval")
           el-select(v-model="editing.leval" placeholder="请选择")
@@ -93,54 +92,30 @@
               v-for="item in designerLeval"
               :key="item._id"
               :label="item.label"
-              :value="{_id: item._id, label: item.label}"
+              :value="item._id"
             )
         el-form-item(label="个人说明" prop="concept")
           el-input(v-model="editing.concept" placeholder="请输入" type="textarea" :autosize="true" :maxlength="250")
         el-form-item(label="头像" prop="avatar")
           img(v-if="editing.avatar" :src="imgPrefix + editing.avatar + '?imageView2/0/w/200/h/200/q/75|imageslim'")
-          el-upload.avatar-uploader(
-            ref="avatarUploader"
-            action="//up.qbox.me/"
-            :show-file-list="false"
-            :thumbnail-mode="true"
-            :on-success="handleSuccess"
-            :before-upload="beforeUpload"
-            :data="postData"
-          )
-            el-button 上传/修改
-            //- img(:src="imgPrefix + editing.avatar")
-            //- .el-upload__text 将文件拖到此处，或<em>点击上传</em>
-            //- el-button 点击上传
-            .el-upload__tip(slot="tip") 只能上传jpg/png文件，且不超过2M
+          uploader(:show-file-list="false" @success="file => this.editing.avatar = file.key")
         el-form-item(label="风格" prop="styles")
           el-checkbox-group(v-model="editing.styles")
             el-checkbox(
               v-for="item in styles"
               :key="item._id"
               :label="item._id"
-              :style="{'margin-left':0, 'margin-right': '10px'}"
+              :style="{'margin': '10px 10px 0 0', 'width': '100px'}"
             ) {{item.label}}
-        el-form-item(label="服务楼盘" v-if="editing.city" prop="services")
-          el-tag(
-            v-for="(item, index) in editing.services"
-            :key="item._id"
-            closable
-            @close="editing.services.splice(index, 1)"
-            :style="{'margin-right': '10px'}"
-          ) {{item.name}}
-          el-dropdown(
-            trigger="click"
-            @command="(item) => editing.services.push(item)"
-          )
-            el-button(type="small") 请添加 <i class="el-icon-arrow-down el-icon--right"></i>
-            el-dropdown-menu.scroll(slot="dropdown")
-              el-dropdown-item(
-                v-for="item in difference(builds, editing.services)"
-                v-if="item.city._id !== editing.city"
-                key="item._id"
-                :command="item"
-              ) {{ item.name }}
+        el-form-item(label="服务楼盘" prop="services" v-if="editing.city")
+          el-checkbox-group(v-model="editing.services")
+            el-checkbox(
+              v-for="item in builds"
+              v-if="item.city._id === editing.city"
+              :key="item._id"
+              :label="item._id"
+              :style="{'margin': '10px 10px 0 0', 'width': '150px'}"
+            ) {{item.name}}
       div.dialog-footer(slot="footer")
         el-button(@click="editVisable = false") 取消
         el-button(type="primary" @click="editSubmit") 确定
@@ -157,13 +132,13 @@
   import _forEach from 'lodash/forEach'
   import _keys from 'lodash/keys'
   import _isEqual from 'lodash/isEqual'
-
+  import uploader from '~/components/admin/uploader.vue'
   const editingInit = {
     name: '',
     avatar: '',
     concept: '',
-    leval: '',
-    city: null,
+    leval: undefined,
+    city: undefined,
     styles: [],
     services: []
   }
@@ -202,28 +177,32 @@
         designerCount: 0,
         editVisable: false,
         editing: editingInit,
-        postData: {
-          token: ''
-        },
-        editRule
+        editRule,
+        test: []
       }
+    },
+    components: {
+      uploader
     },
     methods: {
       difference: _difference,
       edit (item = editingInit) {
         this.editVisable = true
-        this.$nextTick(() => {
+        this.$nextTick(() => {CryptoKey
           this.$refs['editingDialog'].resetFields()
           this.editing = {
             ...item,
             concept: item.concept.replace(/\<br \/\>/g, '\n'),
-            styles: _map(item.styles, '_id')
+            styles: _map(item.styles, '_id'),
+            services: _map(item.services, '_id'),
+            city: item.city ? item.city._id : null,
+            leval: item.leval ? item.leval._id : null
           }
         })
       },
       async del (item, index) {
         try {
-          const comfirm =  await this.$confirm('此操作将永久删除该设计师, 是否继续?', '提示', {
+          const comfirm =  await this.$confirm('将永久删除该设计师, 是否继续？', '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
             type: 'warning'
@@ -279,47 +258,13 @@
         this.designers = data.list
         this.designerCount = data.totalCount
       },
-      handleSuccess (res, file) {
-        this.editing.avatar = res.key
-      },
-      // handleExceed (res) {
-      //   this.$notify.error({
-      //     title: '错误',
-      //     message: '上传失败，超过个数限制。'
-      //   })
-      // },
-
-      async beforeUpload (file) {
-        const isJPG = file.type === 'image/jpeg'
-        const isPNG = file.type === 'image/png'
-        const isLt2M = file.size / 1024 / 1024 < 2
-
-        if (!isJPG&&!isPNG) {
-          this.$message.error('上传头像图片只能是 JPG/PNG 格式!')
-        }
-        if (!isLt2M) {
-          this.$message.error('上传头像图片大小不能超过 2MB!')
-        }
-        const isOk = ( isJPG || isPNG ) && isLt2M
-        if (isOk) {
-          const data = await this.$store.dispatch('getQiniuToken', 'designer/' + uuid.v1() + '.jpg')
-          console.log(data)
-          this.postData = data.data.data
-        }
-        return isOk
-      },
-
       editSubmit () {
         this.$refs['editingDialog'].validate(async valid => {
 
           if (valid) {
             // const isNew  = !this.editing._id
             let edited = {
-              ...this.editing,
-              leval: parseInt(this.editing.leval._id),
-              city: parseInt(this.editing.city._id),
-              services: _map(this.editing.services,item => parseInt(item._id)),
-              styles: _map(this.editing.styles,item => parseInt(item))
+              ...this.editing
             }
             // if (!isNew) {
             //   // is update
@@ -358,9 +303,6 @@
       this.getDesigner(1)
     },
     computed: {
-      editingStyles () {
-        return _map(editing.styles, '_id')
-      },
       ...mapState([
         'user',
         'imgPrefix',
@@ -374,9 +316,15 @@
       currentPage (currentPage) {
         this.getDesigner(currentPage)
       },
-      editing () {
-        // this.$refs['editingDialog'].validate()
+      'editing.avatar' () {
+        this.$nextTick(() => {
+          !_isEqual(this.editing, editingInit) && this.$refs['editingDialog'].validateField('avatar')
+        })
       }
+      // ,
+      // editing () {
+        // this.$refs['editingDialog'].validate()
+      // }
       // ['editing.city'](newCity, oldCity) {
       //   if (oldCity !== newCity) {
       //     this.$nextTick(() => {
