ac23c4ef7028c5be8d79a39ecf1446ea1fcd82f3
weifan she <923986234@qq.com>
2018-01-04 09:01:30 +0800

    update schema

--- server/database/schema/home.js
+++ server/database/schema/home.js
@@ -44,6 +44,30 @@ IndexSwiperSchema.pre('save', setMeta)
 IndexMapSchema.pre('save', setMeta)
 IndexDesignerSchema.pre('save', setMeta)
 
+// 验证效果图id是不是存在
+IndexMapSchema.pre('save', async function (next) {
+  const Maps = mongoose.model('map')
+  const map = await Maps.findOne({_id: this.map}).exec()
+  if (map) {
+    next()
+  } else {
+    const err = new Error('输入的id不存在')
+    next(err)
+  }
+})
+
+// 验证设计师id是不是存在
+IndexDesignerSchema.pre('save', async function (next) {
+  const Designer = mongoose.model('designer')
+  const designer = await Designer.findOne({_id: this.designer}).exec()
+  if (designer) {
+    next()
+  } else {
+    const err = new Error('输入的id不存在')
+    next(err)
+  }
+})
+
 mongoose.model('indexSwiper', IndexSwiperSchema)
 mongoose.model('indexMap', IndexMapSchema)
 mongoose.model('indexDesigner', IndexDesignerSchema)
